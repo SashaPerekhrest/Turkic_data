8ef6202311604046e2e13d3edac368bfcd07fa76
Sashaperekhrest <chebupelmen20@gmail.com>
2024-01-13 13:14:56 +0500

    refactoring triple scripts

--- NSP triple/NSP_triple.py
+++ NSP triple/NSP_triple.py
@@ -4,13 +4,13 @@ import csv
 
 nltk.download('punkt')
 
-file_name = 'file.txt'
+file_name = 'C:/Users/matve/Desktop/workspace/studi/parce/KZ/kz_parced.txt'
 delimiter = '$$$'
 
-file = open(file_name, 'r')
+file = open(file_name, 'r', encoding='utf-8')
 tmp = file.read()
 
-csv_file = open('NSP.csv', 'w', newline="", encoding="utf-8")
+csv_file = open('NSP_kz.csv', 'w', newline="", encoding="utf-8")
 writer = csv.writer(csv_file)
 
 # Записываем заголовки полей
@@ -23,22 +23,32 @@ for textWithUrls in textsWithUrls:
     url = textWithUrls.split("###")[1]
     mainUrl = textWithUrls.split("###")[2]
     sentences = nltk.sent_tokenize(text)
-
-    last = ''
+    tmp = sentences
     iterator = 0
     count = len(sentences)
-    for j in range(0, count , 3):
-        if(j+2 >= count):
+    for j in range(0, count - 1 , 3):
+        if j+2 >= count:
+            continue
+        if j+1 >= count:
+            continue
+        if j >= count:
+            continue
+        if len(sentences[j]) <= 17 or len(sentences[j+1]) <= 17 or len(sentences[j+2]) <= 17:
             continue
         k = random.randint(0, 1)
         if k == 1:
-            writer.writerow([sentences[j], sentences[j+1], sentences[j+2], k , url, mainUrl])
+            writer.writerow([sentences[j].replace('\n',''), sentences[j+1].replace('\n',''), sentences[j+2].replace('\n',''), k , url, mainUrl])
         else:
             pos = random.randint(0, 2)
+            iterator = random.randint(0, len(tmp)-1)
             if pos == 0:
-                writer.writerow([sentences[random.randint(0, count-1)], sentences[j+1], sentences[j+2], k , url, mainUrl])
+                writer.writerow([tmp[iterator].replace('\n',''), sentences[j+1].replace('\n',''), sentences[j+2].replace('\n',''), k , url, mainUrl])
+                del tmp[iterator]
             elif pos == 1:
-                writer.writerow([sentences[j], sentences[random.randint(0, count-1)], sentences[j+2], k , url, mainUrl])
+                writer.writerow([sentences[j].replace('\n',''), tmp[iterator].replace('\n',''), sentences[j+2].replace('\n',''), k , url, mainUrl])
+                del tmp[iterator]
             elif pos == 2:
-                writer.writerow([sentences[j], sentences[j+1], sentences[random.randint(0, count-1)], k , url, mainUrl])
+                writer.writerow([sentences[j].replace('\n',''), sentences[j+1].replace('\n',''), tmp[iterator].replace('\n',''), k , url, mainUrl])
+                del tmp[iterator]
+        count = len(sentences)
 
--- SOP triple/SOP_triple.py
+++ SOP triple/SOP_triple.py
@@ -4,22 +4,19 @@ import csv
 
 nltk.download('punkt')
 
-file_name = 'file.txt'
+file_name = 'C:/Users/matve/Desktop/workspace/studi/parce/KG/kg_parced.txt'
 delimiter = '$$$'
 
-file = open(file_name, 'r')
+file = open(file_name, 'r', encoding="utf-8")
 tmp = file.read()
 
 csv_file = open('SOP.csv', 'w', newline="", encoding="utf-8")
 writer = csv.writer(csv_file)
-
-# Записываем заголовки полей
 writer.writerow(["sentence_A", "sentence_B", "sentence_C", "SOP", "new_url", "main_url"])
 
 textsWithUrls = tmp.split(delimiter)
 
 for textWithUrls in textsWithUrls:
-    print(textWithUrls.split("###"))
     text = textWithUrls.split("###")[0]
     url = textWithUrls.split("###")[1]
     mainUrl = textWithUrls.split("###")[2]
@@ -29,21 +26,23 @@ for textWithUrls in textsWithUrls:
     iterator = 0
     count = len(sentences)
     for j in range(0, count , 3):
-        if(j+2 >= count):
+        if j+2 >= count:
+            continue
+        if len(sentences[j]) <= 17 or len(sentences[j+1]) <= 17 or len(sentences[j+2]) <= 17:
             continue
         k = random.randint(0, 1)
         if k == 1:
-            writer.writerow([sentences[j], sentences[j+1], sentences[j+2], k , url, mainUrl])
+            writer.writerow([sentences[j].replace('\n',''), sentences[j+1].replace('\n',''), sentences[j+2].replace('\n',''), k , url, mainUrl])
         else:
             pos = random.randint(0, 4)
             if pos == 0:
-                writer.writerow([sentences[j], sentences[j+2], sentences[j+1], k , url, mainUrl])
+                writer.writerow([sentences[j].replace('\n',''), sentences[j+2].replace('\n',''), sentences[j+1].replace('\n',''), k , url, mainUrl])
             elif pos == 1:
-                writer.writerow([sentences[j+1], sentences[j], sentences[j+2], k , url, mainUrl])
+                writer.writerow([sentences[j+1].replace('\n',''), sentences[j].replace('\n',''), sentences[j+2].replace('\n',''), k , url, mainUrl])
             elif pos == 2:
-                writer.writerow([sentences[j+1], sentences[j+2], sentences[j], k , url, mainUrl])
+                writer.writerow([sentences[j+1].replace('\n',''), sentences[j+2].replace('\n',''), sentences[j].replace('\n',''), k , url, mainUrl])
             elif pos == 3:
-                writer.writerow([sentences[j+2], sentences[j], sentences[j+1], k , url, mainUrl])
+                writer.writerow([sentences[j+2].replace('\n',''), sentences[j].replace('\n',''), sentences[j+1].replace('\n',''), k , url, mainUrl])
             else:
-                writer.writerow([sentences[j+2], sentences[j+1], sentences[j], k , url, mainUrl])
-
+                writer.writerow([sentences[j+2].replace('\n',''), sentences[j+1].replace('\n',''), sentences[j].replace('\n',''), k , url, mainUrl])
+        count = len(sentences)
